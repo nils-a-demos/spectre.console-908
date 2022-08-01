2f50dde19b51236ad545cfeb29deead4356703e7
Nils Andresen <nils@nils-andresen.de>
2022-08-02 00:16:17 +0200

    initial code

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Spectre.Console;
+using Spectre.Console.Cli;
+
+var app = new CommandApp();
+app.Configure(c =>
+{
+    c.AddCommand<SendNormalizedRecordToLime>("send");
+    c.AddCommand<NormalizeLimeCsvCommand>("normalize");
+});
+return app.Run(args);
+
+internal abstract class BaseCommand : Command
+{
+    public override int Execute(CommandContext context)
+    {
+        AnsiConsole.MarkupLine($"Hello from [green]{GetType().Name}[/].");
+
+        return 0;
+    }
+}
+
+internal sealed class SendNormalizedRecordToLime : BaseCommand
+{
+}
+
+internal sealed class NormalizeLimeCsvCommand : BaseCommand
+{
+}
